ce08aa615a676624033db06877537850f7151f47
Pawan <58373479+pdanani@users.noreply.github.com>
2023-07-21 20:45:25 -0400

    styled demo and github buttons

--- portfolio/components/ProjectCard.js
+++ portfolio/components/ProjectCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { GitHub } from 'react-feather';
 
 const ProjectCard = ({
   title,
@@ -8,17 +8,17 @@ const ProjectCard = ({
 }) => {
   return (
     <div className="bg-black bg-opacity-10 backdrop-filter backdrop-blur-lg shadow-md p-4">
-      <h3 className="text-xl font-semibold text-white">{title}</h3>
-      <p className="text-white">{description}</p>
-      <div className="mt-4">
+      <h3 className="text-2xl font-extrabold text-white font-poppins">{title}</h3>
+      <p className="text-xl text-white font-poppins">{description}</p>
+      <div className="mt-4 flex items-center">
         <a href={demoURL} target="_blank" rel="noopener noreferrer">
-          <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+          <button className="border-2 border-blue-500 hover:border-blue-600 bg-transparent text-blue-500 hover:text-blue-600 font-bold py-2 px-4 rounded font-poppins">
             Demo
           </button>
         </a>
         <a href={githubURL} target="_blank" rel="noopener noreferrer">
-          <button className="bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded ml-2">
-            GitHub
+          <button className="inline-flex items-center justify-center border-2 border-white hover:border-white bg-transparent text-white hover:text-white font-bold py-2 px-4 rounded ml-2 font-poppins">
+            <GitHub className="mr-2 self-center" size={16} /> GitHub
           </button>
         </a>
       </div>
