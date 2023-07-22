4aa5cd13cdccb90ac95b7b4396199c67a0feb557
Pawan <58373479+pdanani@users.noreply.github.com>
2023-07-21 21:16:16 -0400

    Added onHover color change for github button

--- portfolio/components/ProjectCard.js
+++ portfolio/components/ProjectCard.js
@@ -10,14 +10,14 @@ const ProjectCard = ({
     <div className="bg-black bg-opacity-10 backdrop-filter backdrop-blur-lg shadow-md p-4">
       <h3 className="text-2xl font-extrabold text-white font-poppins">{title}</h3>
       <p className="text-xl text-white font-poppins">{description}</p>
-      <div className="mt-4 flex items-center">
+      <div className="mt-4 flex justify-center items-center">
         <a href={demoURL} target="_blank" rel="noopener noreferrer">
           <button className="border-2 border-blue-500 hover:border-blue-600 bg-transparent text-blue-500 hover:text-blue-600 font-bold py-2 px-4 rounded font-poppins">
             Demo
           </button>
         </a>
         <a href={githubURL} target="_blank" rel="noopener noreferrer">
-          <button className="inline-flex items-center justify-center border-2 border-white hover:border-white bg-transparent text-white hover:text-white font-bold py-2 px-4 rounded ml-2 font-poppins">
+          <button className="inline-flex items-center justify-center border-2 border-white hover:border-gray-500 bg-transparent text-white hover:text-gray-500 font-bold py-2 px-4 rounded ml-2 font-poppins">
             <GitHub className="mr-2 self-center" size={16} /> GitHub
           </button>
         </a>
