cf18dd8285b0b45549afebe103b08b4f03f0ec17
Pawan <58373479+pdanani@users.noreply.github.com>
2023-07-26 20:01:54 -0400

    Updated Project card to center project description and formatting

--- portfolio/components/ProjectCard.js
+++ portfolio/components/ProjectCard.js
@@ -8,11 +8,11 @@ const ProjectCard = ({
 }) => {
   return (
     <div className="bg-black bg-opacity-10 backdrop-filter backdrop-blur-lg shadow-md p-4 flex flex-col h-full">
-      <div>
-        <h3 className="text-2xl font-extrabold text-white font-poppins">{title}</h3>
-        <p className="text-md text-white font-poppins">{description}</p>
+      <h3 className="text-2xl font-extrabold text-white font-poppins">{title}</h3>
+      <div className="flex-grow flex flex-col justify-center">
+        <p className="text-sm text-white font-poppins pt-2">{description}</p>
       </div>
-      <div className="mt-auto flex justify-center items-center pt-2">
+      <div className="mt-auto flex justify-center items-center pt-3">
         {demoURL && 
           <a href={demoURL} target="_blank" rel="noopener noreferrer">
             <button className="border-2 border-blue-500 hover:border-blue-600 bg-transparent text-blue-500 hover:text-blue-600 font-bold py-2 px-4 rounded font-poppins">
